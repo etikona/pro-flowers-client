d9150351be6699795299105384ec19270b98e959
eti kona <etikonapal@gmail.com>
2022-11-08 03:31:56 +0600

    context setup completed

---
+++ src/components/context/AuthProvider.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from 'react';
+
+
+  export  const AuthContext = createContext();
+ 
+const AuthProvider = ({children}) => {
+        const [user, setUser] = useState(null);
+        const [loader, setLoader] = useState(true);
+
+    const authInfo = {
+
+    }
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthProvider;
